a460c31ad970469b62356bd3fbdc76a2817aac16
Thiago Bruno <tbgt35@hotmail.com>
2022-10-10 16:54:07 -0300

    Testando o comando git branch

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -10,6 +10,7 @@ root.render(
     <App />
   </React.StrictMode>
 );
+//Testando o merge da minha branch : Bruno
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
