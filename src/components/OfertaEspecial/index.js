17ec6aae629087c773f3d59bc154de6b979c5c68
Ciro <cbjales@gmail.com>
2022-10-12 13:26:49 -0300

    Edit: componente OfertaEspecial

--- src/components/OfertaEspecial/index.js
+++ src/components/OfertaEspecial/index.js
@@ -1,31 +1,33 @@
 import "./style.css";
-import tenis from "./tenis-oferta-especial.svg";
-import elipse from "./elipse-tenis.svg";
+import elipse from "../../assets/OfertaEspecial/elipse-tenis.svg";
+import tenis from "../../assets/OfertaEspecial/tenis-oferta-especial.svg";
 
 export default function OfertaEspecial() {
   return (
     <section className="oferta-especial">
-      <div className="esquerda">
+      <div style={{ backgroundImage: `url(${elipse})` }} className="esquerda">
         <img className="tenis" src={tenis} alt="" />
-        <img className="elipse" src={elipse} alt="" />
+        {/* <img className="elipse" src={elipse} alt="" /> */}
       </div>
       <div className="direita">
         <div className="wrapper-texto1">
-          <span className="texto1">Oferta especial</span>
+          <p className="texto1">Oferta especial</p>
         </div>
         <div className="wrapper-texto2">
-          <p className="texto2">Air Jordan edição de colecionador</p>
+          <h2 className="texto2">Air Jordan edição de colecionador</h2>
         </div>
         <div className="wrapper-texto3">
           <p className="texto3">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do br
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-            minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
             aliquip
           </p>
         </div>
         <div className="wrapper-button">
-          <button>Ver Oferta</button>
+          <a href="">
+            <button className="botao-ver-oferta">Ver Oferta</button>
+          </a>
         </div>
       </div>
     </section>
