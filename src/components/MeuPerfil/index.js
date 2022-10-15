4b0caddb84f9018a7a12cfbf183c1df0313923b9
Sergio Ferreira <Sergio.ferreira.tavares.filho@gmail.com>
2022-10-15 00:01:47 -0300

    Editando meu Perfil

--- src/components/MeuPerfil/index.js
+++ src/components/MeuPerfil/index.js
@@ -3,7 +3,12 @@ import "./style.css"
 export default function MeuPerfil() {
     return (
         <div className="MeuPerfil-body">
-            
+            <span className="MeusPedidos-span">
+                Meu Perfil<hr className="MeuPerfil-hr"/>
+                <p className="MeusPedidos-cor">Meus Pedidos <hr className="MeuPerfil-hr"/></p>
+                Minhas informações <hr className="MeuPerfil-hr"/>
+                Metodos Pagamentos <hr className="MeuPerfil-hr"/>
+            </span>
         </div>
     )
 }
